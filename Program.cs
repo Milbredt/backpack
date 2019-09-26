6ac24d1a207ccea26be58ebc69e802dfaa11150a
Filip Milbredt <filip_milbredt@hotmail.com>
2019-09-26 16:24:04 +0200

    dotnet new

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace backpack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
